1bda5e5a82422e9f255db19bd9acff5a079474b0
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-11-12 18:24:12 -0700

    Comment out first step of JS code

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
 
-var section = document.getElementsByTagName('section');
-var button = document.getElementsByTagName('button');
+//var section = document.getElementsByTagName('section');
+//var button = document.getElementsByTagName('button');
 
-function slideToggle () {
-    section.classList.add('hide');
-};
+//function slideToggle () {
+    //section.classList.add('hide');
+//};
 
-for (i=0; i < button.length; i++) {
- button[0].addEventListener('click', slideToggle);
-};
+//for (i=0; i < button.length; i++) {
+    //button[0].addEventListener('click', slideToggle);
+//};
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
