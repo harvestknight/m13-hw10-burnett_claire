41b49fe02c7791309754429f8a5a9581a34963da
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-11-12 18:40:37 -0700

    Comment out Part 1 JS and return to Part 2

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
 
-var section = document.getElementsByTagName('section');
-var button = document.getElementsByTagName('button');
+//var section = document.getElementsByTagName('section');
+//var button = document.getElementsByTagName('button');
 
-function slideToggle () {
-    section[0].classList.toggle('hide');
+//function slideToggle () {
+    //section[0].classList.toggle('hide');
     
-};
+//};
 
-for (i=0; i < button.length; i++) {
-    button[0].addEventListener('click', slideToggle);
-};
+//for (i=0; i < button.length; i++) {
+    //button[0].addEventListener('click', slideToggle);
+//};
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
